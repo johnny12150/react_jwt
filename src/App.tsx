9b71033b50dcea5937e0bd52e5f335b12d284dc8
wade <johnny12150@gmail.com>
2024-03-07 20:33:19 +0800

    Signup to backend using fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+interface User {
+  name: string;
+}
+
+const fetchLogin = async () => {
+  try {
+    // add {credentials: 'include'} to set cookie
+    const response = await fetch('http://localhost:4000/test_signup', {credentials: 'include'});
+    // if (!response.ok) {
+    //   throw new Error('Network response was not ok');
+    // }
+    const token = await response.json();
+    return 'token' in token;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+    throw error; // Rethrow the error if you want to handle it outside
+  }
+};
+
+const fetchUserData = async () => {
+  try {
+    const response = await fetch('http://localhost:4000/login');
+    // if (!response.ok) {
+    //   throw new Error('Network response was not ok');
+    // }
+    const user_data = await response.json();
+    return user_data.name;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+    throw error; // Rethrow the error if you want to handle it outside
+  }
+};
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    fetchLogin()
+        .then(setIsLogin)
+        .catch((error) => setError(error.message));
+  }, [isLogin]);
+
+  useEffect(() => {
+    fetchUserData()
+        .then(setUser)
+        .catch((error) => setError(error.message))
+        .finally(() => setLoading(false));
+  }, [isLogin]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!user) return <div>No user data found.</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>{user.name}</h1>
+      </div>
   );
 }
 
