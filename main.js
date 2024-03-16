d85646c1cc6bcf46096602c6f1edec9ee85c3eff
wade <johnny12150@gmail.com>
2024-03-16 15:00:46 +0800

    Use build file replace url

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function createWindow() {
         }
     });
 
-    // mainWindow.loadURL(`file://${path.join(__dirname, "/build/index.html")}`);
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL(`file://${path.join(__dirname, "/build/index.html")}`);
+    // mainWindow.loadURL('http://localhost:3000');
 
     mainWindow.on("closed", () => (mainWindow = null));
 }
